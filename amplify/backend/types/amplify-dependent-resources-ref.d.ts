8a78ecf1c1252e185b82b948fed4fa39c19863ab
Jeffrey <jeffreykblau@gmail.com>
2024-01-24 20:41:59 +0800

    node 18 update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "photoshareb143529b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger984fb593": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "photostorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
